1ccf8492d1cbda943cbaa51ad491532cbc942590
Logarithmus <logarithmus.dev@gmail.com>
2019-02-10 08:29:43 +0300

    1. Bugfix: page reload required to ban or delete yourself (it is due to the fact that database commit happens after the page load) 2. Change all toolbar buttons to btn-secondary, because some people don't like colorful buttons 3. Modify "ban" and add "delete" page 4. Drop separate MariaDB in favor for embedded H2 datatabase

--- src/main/java/socialcrud/controllers/UsersController.java
+++ src/main/java/socialcrud/controllers/UsersController.java
@@ -1,5 +1,15 @@
 package socialcrud.controllers;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +22,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-
 import socialcrud.User;
 import socialcrud.UserRepository;
 
@@ -27,25 +30,54 @@ public class UsersController {
     @Autowired 
     private UserRepository userRepository;
 
+    private String handleUserBanOrDelete(User user, HttpServletRequest request) {
+        Optional<User> dbUser = userRepository.findById(user.getId());
+        boolean isBanned = dbUser.isPresent() && dbUser.get().getIsBanned();
+        if (isBanned) {
+            new SecurityContextLogoutHandler().logout(request, null, null);
+            return "forward:/banned";
+        }
+        boolean isDeleted = dbUser.isEmpty();
+        if (isDeleted) {
+            new SecurityContextLogoutHandler().logout(request, null, null);
+            return "forward:/deleted";
+        }
+        return null;
+    }
+
     @RequestMapping("/users")
     public String users(SecurityContextHolder context, HttpServletRequest request, Model model,
                        @RequestParam(required = false) String action,
                        @RequestParam(required = false) ArrayList<String> checkbox) {
 
-        var currentUser = (User)SecurityContextHolder.getContext()
+        Logger logger = LoggerFactory.getLogger(UsersController.class);
+
+        User currentUser = (User)SecurityContextHolder.getContext()
                         .getAuthentication().getPrincipal();
-        Optional<User> dbUser = userRepository.findById(currentUser.getId());
-        boolean isBanned = dbUser.isPresent() && dbUser.get().getIsBanned();
-        if (isBanned || dbUser.isEmpty()) {
-            new SecurityContextLogoutHandler().logout(request, null, null);
-            return "redirect:/banned";
+        String viewName = handleUserBanOrDelete(currentUser, request);
+        if (viewName != null) {
+            return viewName;
         }
 
         if ((action != null) && (checkbox != null)) {
             switch (action) {
-                case "delete": checkbox.forEach(id -> userRepository.deleteById(id)); break;
-                case "ban":    checkbox.forEach(id -> userRepository.updateIsBanned(id, true)); break;
-                case "unban":  checkbox.forEach(id -> userRepository.updateIsBanned(id, false)); break;
+                case "delete":
+                    checkbox.forEach(id -> userRepository.deleteById(id)); 
+                    if (checkbox.contains(currentUser.getId())) {
+                        new SecurityContextLogoutHandler().logout(request, null, null);
+                        return "forward:/deleted";
+                    }
+                    break;
+                case "ban":
+                    checkbox.forEach(id -> userRepository.updateIsBanned(id, true));
+                    if (checkbox.contains(currentUser.getId())) {
+                        new SecurityContextLogoutHandler().logout(request, null, null);
+                        return "forward:/banned";
+                    }
+                    break;
+                case "unban":
+                    checkbox.forEach(id -> userRepository.updateIsBanned(id, false));
+                    break;
             }
         }
         
@@ -55,8 +87,13 @@ public class UsersController {
         return "users";
     }
     
-    @GetMapping("/banned")
+    @RequestMapping("/banned")
     public String banned() {
         return "banned";
+    }
+
+    @RequestMapping("/deleted")
+    public String deleted() {
+        return "deleted";
     }
 }
